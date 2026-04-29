083e6d8a9732c39077e68218fdd0c3a5956c600b
Quentin Gouttaya <beatsgr3y@gmail.com>
2026-04-29 23:30:15 +0200

    Feature/EDT-106: no jwt, quiz list ok

--- config/settings.py
+++ config/settings.py
@@ -82,13 +82,14 @@ AUTH_PASSWORD_VALIDATORS = [
 
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
-        "rest_framework_simplejwt.authentication.JWTAuthentication",
+        "user.auth.CsrfExemptSessionAuthentication",
     ],
     "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.permissions.IsAuthenticated",
     ],
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
 }
+
 
 ROOT_URLCONF = "config.urls"
 
