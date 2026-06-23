cfd085641c9ba16e37815fd4204f9b02b9fc86af
mohamedmaghzaoui <mohamedmaghzaoui53@gmail.com>
2026-06-24 00:15:32 +0200

    fix: import csv saved with excel EDT-76

--- content/constants.py
+++ content/constants.py
@@ -14,15 +14,18 @@ QUESTION_HEADER = [
 ]
 
 QUESTION_TYPE_TO_FR = {
-    "single_choice": "Choix unique",
+    "single_choice":   "Choix unique",
     "multiple_choice": "Choix multiple",
-    "true_false": "Vrai / Faux",
+    "true_false":      "Vrai / Faux",
 }
 
-QUESTION_TYPE_FROM_FR = {
-    "Choix unique": "single_choice",
-    "Choix multiple": "multiple_choice",
-    "Vrai / Faux": "true_false",
+QUESTION_TYPE_FROM_ANY = {
+    "choix unique": "single_choice",
+    "choix multiple": "multiple_choice",
+    "vrai / faux": "true_false",
+    "single_choice": "single_choice",
+    "multiple_choice": "multiple_choice",
+    "true_false": "true_false",
 }
 
 DIFFICULTY_TO_FR = {
@@ -31,20 +34,27 @@ DIFFICULTY_TO_FR = {
     "hard": "Difficile",
 }
 
-DIFFICULTY_FROM_FR = {
-    "Facile": "easy",
-    "Moyen": "medium",
-    "Difficile": "hard",
+DIFFICULTY_FROM_ANY = {
+    "facile": "easy",
+    "moyen": "medium",
+    "difficile": "hard",
+    "easy": "easy",
+    "medium": "medium",
+    "hard": "hard",
 }
 
 BOOL_TO_FR = {
-    True: "Vrai",
+    True:  "Vrai",
     False: "Faux",
 }
 
-BOOL_FROM_FR = {
+BOOL_FROM_ANY = {
     "vrai": True,
+    "oui":  True,
     "true": True,
+    "1":  True,
     "faux": False,
+    "non":  False,
     "false": False,
+    "0": False,
 }
